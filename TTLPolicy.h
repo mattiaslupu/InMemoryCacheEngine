ace2deb04d183a0eb22646d26b46f37f37003f49
Mattias <mattias.lupu@yahoo.com>
2026-05-17 00:22:05 +0300

    feat: added constructors, destructor, setters, getters and methods for TTLPolicy  class

---
+++ TTLPolicy.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "EvictionPolicy.h"
+#include <unordered_map>
+#include <string>
+#include <ctime>
+
+class TTLPolicy : public virtual EvictionPolicy {
+private:
+    std::unordered_map<std::string, time_t> expirations;
+    time_t defaultTTL;
+
+public:
+    TTLPolicy();
+    TTLPolicy(time_t defaultTTL);
+    TTLPolicy(const TTLPolicy& obj);
+
+    TTLPolicy& operator=(const TTLPolicy& obj);
+
+    ~TTLPolicy() override;
+
+    void onAccess(const std::string& key) override;
+    void onInsert(const std::string& key) override;
+    void onRemove(const std::string& key) override;
+    std::string evict() override;
+    std::string getName() const override;
+    void reset() override;
+
+    bool isExpired(const std::string& key) const;
+
+    void setDefaultTTL(time_t seconds);
+    time_t getDefaultTTL() const;
+    friend std::ostream& operator<<(std::ostream& os, const TTLPolicy& obj);
+};
